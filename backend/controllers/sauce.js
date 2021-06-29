35ff10aaccd40136cb6918056c419f3714693b0f
Nathan Akpawu <nakpawu@gmail.com>
2021-06-29 16:15:29 +0000

    Get all sauces

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,9 +1,13 @@
 const Sauce = require('../models/Sauce');
 
+// Ajouter une nouvelle sauce
 exports.createSauce = (req, res, next) => {
     res.json('Je teste la création d\'une sauce');
 }
 
+// Afficher toutes les sauces
 exports.getAllSauces = (req, res, next) => {
-    res.json('Je teste la récupération de toutes les sauces');
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
 }
